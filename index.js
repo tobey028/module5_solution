8a56b60254f643d6821814662c1d4cc1b5d04999
tobey028 <168511064+tobey028@users.noreply.github.com>
2024-06-06 21:45:22 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  // Fetch all category short names (replace with your server-side logic)
+  var categoryShortNames = ['CHKN', 'BEEF', 'SUSHI']; // Example data
+
+  
